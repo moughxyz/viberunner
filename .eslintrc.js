3c49da0f7c5b2c535957270f8f415c384be18a34
moughxyz <mo@standardnotes.com>
2025-06-01 14:21:59 -0500

    Add ESLint configuration for TypeScript + React + Electron project

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  ignorePatterns: [
+    'dist',
+    'dist-electron',
+    'out',
+    'node_modules',
+    '.eslintrc.js',
+    'forge.config.js',
+    'vite.config.ts',
+    'ExampleApps/**/*'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react-hooks', 'react-refresh'],
+  globals: {
+    Electron: 'readonly',
+    NodeRequire: 'readonly',
+    NodeModule: 'readonly',
+    NodeJS: 'readonly',
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-undef': 'error',
+    'no-case-declarations': 'error',
+    'no-prototype-builtins': 'error',
+    'no-redeclare': 'error',
+    'no-unused-vars': 'off',
+    'no-unsafe-optional-chaining': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.d.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+}
