c6d8c99fe435bd2891796ba605652ace69ebb246
moughxyz <mo@standardnotes.com>
2025-06-01 15:54:59 -0500

    Remove unused apps

--- ExampleApps/dotfile-editor/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import App from './App.tsx'
-
-// Export the component for Viberunner to load
-export default App;
-
-// Global registration for IIFE bundle
-if (typeof window !== 'undefined' && (window as any).__LOAD_APP__) {
-  (window as any).__LOAD_APP__(App);
-}
